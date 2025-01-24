06b63c1846db664ce3f10172d958ceb7168cbb13
KillianRD <killdutilleul@gmail.com>
2025-01-24 08:00:50 +0100

    Feat : Ajout des statistiques dans le main

--- SAE/src/LancerRecherche.java
+++ SAE/src/LancerRecherche.java
@@ -1,29 +1,44 @@
 import ia.framework.MLP.MLP;
 import ia.framework.common.ArgParse;
+import ia.framework.common.Statistique;
+import ia.framework.utils.FileUtils;
 import ia.problemes.Problem;
 
 public class LancerRecherche {
     public static void main(String[] args) {
 
         ArgParse.setUsage
-                ("Utilisation :\n\n"
-                        + "java LancerRecherche [-prob problem] [-func functionActivatation]"
-                        + " [-layers {int}] [-lr double] [-debug]\n\n"
-                        + "-prob : Le nom du problem {OR, AND, XOR, MNIST, FashionMNIST}. Par défaut OR\n"
-                        + "-func : fonction d'activation {sigmoid, tanh}. Par défault sigmoid\n"
-                        + "-layers : le nombre de neurones du réseau par couche. Par défaut 2 couches de taille 2 neurones\n"
-                        + "-lr : taux d'apprentissage. Par défaut 0.1\n"
-                        + "-debug    : Rendre bavard (mettre à la fin)\n"
+                ("""
+                        Utilisation :
+
+                        java LancerRecherche [-prob problem] [-nbIteration int] [-func functionActivatation]\
+                         [-layers {int}] [-lr double] [-debug] [-stats]
+
+                        -prob : Le nom du problem {OR, AND, XOR, MNIST, FashionMNIST}. Par défaut OR
+                        -nbIteration : nombre d'itérations pour l'entrainement du mlp
+                        -func : fonction d'activation {sigmoid, tanh}. Par défault sigmoid
+                        -layers : le nombre de neurones du réseau par couche. Par défaut {2, 2, 1}
+                        -lr : taux d'apprentissage. Par défaut 0.1
+                        -debug : Rendre bavard (mettre à la fin)
+                        -stats : Permet de rendre les résultats du MLP sur un problème
+                        """
                 );
+
 
         ArgParse.handleFlags(args);
 
         String prob_name = ArgParse.getProbFromCmd(args);
         String func_name = ArgParse.getActivationFuncFromCmd(args);
+        int nbIteration = ArgParse.getValueOfParam(args, "-nbIteration", 10);
 
         MLP mlp = ArgParse.makeMLP(args, func_name);
         Problem p = ArgParse.makeProblem(prob_name, mlp);
+        Statistique s = ArgParse.makeStats(args, mlp, p);
 
-        p.executeTraining(10_000);
+        p.executeTraining(nbIteration, mlp);
+
+        if (s != null) {
+            FileUtils.saveStatistique(s.getStatistiquePerClasse());
+        }
     }
 }
